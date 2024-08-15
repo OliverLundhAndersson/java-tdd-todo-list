a5a79f9a40cc3f6ae912cc13dd2e2f0fa684896f
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-08-15 09:39:35 +0200

    addTask pass

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -6,7 +6,23 @@ public class TodoList {
     private ArrayList<Task> tasks = new ArrayList<>();
 
     public boolean addTask(String name) {
+        if (tasks.isEmpty()) {
+            Task task = new Task(name);
+            tasks.add(task);
+            return true;
+        }
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).name.equals(name)) {
+                System.out.println("A task with that name already exists");
+                return false;
+            } else if (tasks.get(i).name.compareTo(name) > 0) {
+                Task task = new Task(name);
+                tasks.add(i, task);
+                return true;
+            }
+        }
         Task task = new Task(name);
+        tasks.add(task);
         return true;
     }
 }
