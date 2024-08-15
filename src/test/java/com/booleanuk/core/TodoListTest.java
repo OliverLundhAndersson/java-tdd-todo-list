eb727631a91d978a30e2521faca09b8f7dc0182f
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-08-15 09:46:00 +0200

    viewTask fail

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,18 +3,26 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class TodoListTest {
     public TodoList todoList;
     @Test
-    public void addTask() {
+    public void testAddTask() {
         todoList = new TodoList();
         boolean add1 = todoList.addTask("test");
         assertTrue(add1);
 
         boolean add2 = todoList.addTask("test");
         assertFalse(add2);
+    }
+
+    @Test
+    public void testViewTask() {
+        todoList = new TodoList();
+        ArrayList<Task> view1 = todoList.viewTask();
+        assertEquals(view1.size(), 0);
     }
 }
